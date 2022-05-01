2c0c8905ea0e95114596b4aa3110fa0f6c760ab9
SitaRamSai <sitaramsai.koya20.com>
2022-05-01 04:08:25 -0500

    working code

--- team1/user/src/App.js
+++ team1/user/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route } from 'react-router';
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import LoggedInHome from './components/LoggedInHome';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends React.Component{
+
+ render() {
+  return(
+    <div>
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route path='/home' element={<LoggedInHome/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<SignUp/>} />
+      </Routes>
     </div>
-  );
+  )}
 }
 
 export default App;
