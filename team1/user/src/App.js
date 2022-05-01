13643c6488271cc06fa1db3eceacb2ac8e6d7e1c
SitaRamSai <sitaramsai.koya20.com>
2022-04-30 20:16:08 -0500

    fix bugs

--- team1/user/src/App.js
+++ team1/user/src/App.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import { Routes, Route } from 'react-router';
+import logo from './logo.svg';
 import './App.css';
-import About from './components/About';
-import Home from './components/Home';
-import LoggedInHome from './components/LoggedInHome';
-import Login from './components/Login';
-import Signup from './components/Signup';
 
-
-class App extends React.Component{
-
- render() {
-  return(
-    <div>
-      <Routes>
-        <Route exact path='/' element={<Home/>} />
-        <Route path='/home' element={<LoggedInHome/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/signup' element={<Signup/>} />
-      </Routes>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )}
+  );
 }
 
 export default App;
