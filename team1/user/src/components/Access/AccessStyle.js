ae0dff5b25928b84b123e14b40fd177a906d6e5c
Brandon May <brandonmay687@gmail.com>
2022-05-24 13:22:51 -0400

    builds login and signup pages

---
+++ team1/user/src/components/Access/AccessStyle.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+// Login Styles
+export const LoginDiv = styled.div`
+
+`;
+
+export const LoginForm = styled.form`
+
+`;
+
+export const LoginLabel = styled.label`
+
+`;
+
+export const Red = styled.span`
+    color: red;
+`;
+
+export const LoginInput = styled.input`
+
+`;
+
+export const LoginSubmitBtn = styled.button`
+
+`;
+
+// Signup Styles
+
+export const SignupDiv = styled.div`
+
+`;
+export const SignupForm = styled.form`
+
+`;
+export const SignupLabel = styled.label`
+
+`;
+export const SignupInput = styled.input`
+
+`;
+export const SignupSubmitBtn = styled.button`
+
+`;
